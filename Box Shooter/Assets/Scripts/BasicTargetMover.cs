739b61378c0b1029a0444945b1ab9dfc593017de
rtds8 <64678924+rtds8@users.noreply.github.com>
2021-02-16 14:26:48 +0530

    Game Play Setup
    Player, Target, UI and other basic gameplay things are setup.

---
+++ Box Shooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    public float _spinSpeed = 180.0f;
+    public float _motionMagnitude = 0.1f;
+    public bool _doSpin = true;
+    public bool _doMotion = false;
+
+    void Update()
+    {
+        if (_doSpin)
+        {
+            //rotate around y-axis
+            gameObject.transform.Rotate(Vector3.up * _spinSpeed * Time.deltaTime);
+        }
+
+        if (_doMotion)
+        {
+            //move up & down 
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * _motionMagnitude);
+        }
+        
+    }
+}
